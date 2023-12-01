aca7496aecf91107022af54a87aea6b106506524
PatriciaYo <55708435+PatriciaYo@users.noreply.github.com>
2023-11-30 18:13:47 -0600

    Create 3. Encuentre el factorial de cualquier nú.py

---
+++ 3. Encontrar el factorial de cualquier nú.py
@@ -0,0 +1,8 @@
+# Encuentre el factorial de cualquier número dado
+import math
+
+
+print("Introducir número entero positivo para calcular factorial")
+numero = int(input("Numero: "))
+factorial= math.factorial(numero)
+print(factorial)
